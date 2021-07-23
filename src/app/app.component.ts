dc6fec9208c1961c1cfeca88ffc684d6739b5cd6
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-07-23 16:50:28 +0300

    added Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,42 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularNorthwindDemo';
   user = 'Tarık';
-  product1 = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 1,
-    unitprice: 5,
-  };
-  product3 = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 1,
-    unitprice: 5,
-  };
-  product2 = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 1,
-    unitprice: 5,
-  };
-  product4 = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 1,
-    unitprice: 5,
-  };
-  product5 = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 1,
-    unitprice: 5,
-  };
-
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  product1 = {
+    productId: 1,
+    productName: 'bardak',
+    categoryId: 1,
+    unitPrice: 5,
+  };
+  product3 = {
+    productId: 2,
+    productName: 'maşa',
+    categoryId: 2,
+    unitPrice: 6,
+  };
+  product2 = {
+    productId: 3,
+    productName: 'fincan',
+    categoryId: 1,
+    unitPrice: 489,
+  };
+  product4 = {
+    productId: 4,
+    productName: 'ızgara',
+    categoryId: 2,
+    unitPrice: 78,
+  };
+  product5 = {
+    productId: 5,
+    productName: 'tepsi',
+    categoryId: 13,
+    unitPrice: 55,
+  };
+
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5,
+  ];
   constructor() { }
 
   ngOnInit(): void {
