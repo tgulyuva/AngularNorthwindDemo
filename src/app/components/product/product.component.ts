efcd4897b8d6724460718f5fc5ced6f88f04c8a9
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-08-05 01:27:03 +0300

    ürünlerin compenenti web api ile bağlandı. Ürünler db den geliyor.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-product',
@@ -8,10 +10,25 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
 
-  products:Product[] = [];
-  constructor() { }
+  products: Product[] = [];
+  apiUrl = "https://localhost:44392/api/Products/getall"
+  productResponseModel: ProductResponseModel = {
+    data: this.products,
+    message: "",
+    success: true
+
+  };
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getproducts();
+  }
+
+  getproducts() {
+    this.httpClient.get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data
+      });
   }
 
 }
